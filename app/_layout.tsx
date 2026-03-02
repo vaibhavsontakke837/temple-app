ceadc354cb127ee61566576209b5ebf979f8ac35
Vaibhav Sontakke <98118885+vaibhavsontakke837@users.noreply.github.com>
2026-03-03 00:33:15 +0530

    Implementation of language store when page refresh
    Implementation of language store when page refresh

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,23 @@
 import { Drawer } from "expo-router/drawer";
 import AppHeader from "../components/mobile/AppHeader";
 import CustomDrawer from "../components/mobile/CustomDrawer";
+import { LanguageProvider } from "../context/LanguageContext";
 import { TempleProvider } from "../context/TempleContext";
 import { ThemeProvider } from "../context/ThemeContext";
 import "../i18n";
 
 export default function Layout() {
   return (
-    <ThemeProvider>
-      <TempleProvider>
-      <Drawer
-        initialRouteName="Home"
-        drawerContent={(props) => <CustomDrawer {...props} />}
-        screenOptions={{ header: () => <AppHeader /> }}
-      />
-      </TempleProvider>
-    </ThemeProvider>
+    <LanguageProvider>
+      <ThemeProvider>
+        <TempleProvider>
+          <Drawer
+            initialRouteName="Home"
+            drawerContent={(props) => <CustomDrawer {...props} />}
+            screenOptions={{ header: () => <AppHeader /> }}
+          />
+        </TempleProvider>
+      </ThemeProvider>
+    </LanguageProvider>
   );
 }
