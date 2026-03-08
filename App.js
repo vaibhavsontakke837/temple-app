3e1e0744026d81ef0b3305b00ca7ec8e068a0f32
Vaibhav Sontakke <98118885+vaibhavsontakke837@users.noreply.github.com>
2026-03-08 15:19:43 +0530

    language fixes
    language fixes

--- App.js
+++ App.js
@@ -1,32 +1,8 @@
 import "expo-router/entry";
-import * as Notifications from "expo-notifications";
-import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { initEventNotifications } from "./utils/initEventNotifications";
-import { setupNotifications } from "./utils/notifications";
-
 import { ThemeProvider } from "./context/ThemeProvider";
 
-// Configure notification handler
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-  }),
-});
-
 export default function App() {
-  useEffect(() => {
-    async function init() {
-      const granted = await setupNotifications();
-      if (granted) {
-        await initEventNotifications();
-      }
-    }
-    init();
-  }, []);
-
   return (
     <ThemeProvider>
       <SafeAreaProvider />
