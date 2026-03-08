6297cdfff72f201767ad90d50cbedeee3f769ead
Vaibhav Sontakke <98118885+vaibhavsontakke837@users.noreply.github.com>
2026-03-08 14:04:24 +0530

    rnlish marathi translation fixes
    rnlish marathi translation fixes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -19,7 +19,7 @@ export default function HomeScreen() {
       <TimingCard />
 
       <SectionHeader title={t("upcomingEvents")} />
-      <EventsList limit={4} />
+      <EventsList limit={6} />
     </ScreenContainer>
   );
 }
